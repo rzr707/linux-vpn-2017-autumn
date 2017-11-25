354b0113ee6b9ef0ef3d0803b3b1e113bc58de50
Ivan Kuzmov <rzr707@gmail.com>
2017-11-25 23:12:19 +0200

    Added TunnelManager::log compatibility with GCC < 5.0.0

--- VPN_Server/src/tunnel_mgr.hpp
+++ VPN_Server/src/tunnel_mgr.hpp
@@ -18,6 +18,10 @@
 #include <sys/types.h>
 #include <ifaddrs.h>
 
+#define GCC_VERSION (__GNUC__ * 10000 \
+                     + __GNUC_MINOR__ * 100 \
+                     + __GNUC_PATCHLEVEL__ * 10)
+
 /**
  * @brief The TunnelManager class
  * Contains tunnel set of currently using tunnels
@@ -177,55 +181,6 @@ public:
         // SaveTunToFile(tunName);
     }
     
-    /*
-    void SaveTunToFile(const std::string& TunName)
-    {
-        save_tun_file.open(SaveTunListFilename, std::ios_base::out | std::ios_base::app);
-        if(!save_tun_file.is_open()) {
-            throw std::runtime_error(std::string() + "TunnelManager::SaveTunToFile - Could not open file to save tun information");
-        }
-        
-        save_tun_file << TunName << std::endl;
-        save_tun_file.close();
-    }
-    
-    void DelTunFromFile(const std::string& TunName, bool ClearAllData = false)
-    {
-        int tun_length = 0;
-        int position = 0;
-        std::string line;
-        
-        save_tun_file.open(SaveTunListFilename, std::ios::in | std::ios::out);
-        if(!save_tun_file.is_open()) {
-            throw std::runtime_error(std::string() + "TunnelManager::SaveTunToFile - Could not open file to save tun information");
-        }
-        
-        if(!ClearAllData)
-        {
-            while(getline(save_tun_file, line)) {  
-                if(line[0] != ' ' && !strcmp(line.c_str(), TunName.c_str()))
-                {
-                    save_tun_file.seekg(position);
-                    tun_length = line.length();
-                    while(tun_length--)
-                        save_tun_file << ' ';
-                    break;
-                }
-                else
-                    position+=line.length()+1;
-            }
-        }
-        else
-        {
-            while(getline(save_tun_file, line)) 
-                if(line[0] != ' ')
-                    execTerminalCommand("ip link delete " + line);
-                
-            cleanFile(SaveTunListFilename);
-        }
-        
-        save_tun_file.close();
-    } */
 
     /**
      * @brief cleanupTunnels method cleans uplinked tunnels previous<br>
@@ -248,6 +203,17 @@ public:
     }
 
     /**
+     * @brief currentTime
+     * @return string with time in format "<WWW MMM DD hh:mm:ss yyyy>"
+     */
+    static std::string currentTime() {
+        auto currTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        std::string time = std::string() +  "<" + ctime(&currTime);
+        time = time.substr(0, time.length() - 1) + ">";
+        return time;
+    }
+
+    /**
      * @brief log - log out the message to output stream 's'
      * (needs to be reimplemented as separate
      *  class in log.hpp or utils.hpp)
@@ -256,7 +222,9 @@ public:
      */
     static void log(const std::string& msg,
                     std::ostream& s = std::cout) {
-
+// Check if GCC ver. bigger or equals 5.0.0
+// (because there is no std::put_time on older versions):
+#if GCC_VERSION >= 50000
         auto timeNow = std::chrono::system_clock::now();
         auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
                     timeNow.time_since_epoch()
@@ -265,13 +233,17 @@ public:
         std::time_t currTime
                 = std::chrono::system_clock::to_time_t(timeNow);
 
-
         s << std::put_time(std::localtime(&currTime), "<%d.%m.%y %H:%M:%S.")
           << ms.count() << '>'
           << " <THREAD ID: " << std::this_thread::get_id()  << '>'
           << ' '
           << msg << std::endl;
+#else
+        s << currentTime() << ' '
+          << msg << std::endl;
+#endif
     }
+
 };
 
 #endif // TUNNEL_MGR_HPP
